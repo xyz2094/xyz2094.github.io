ed1954e3f1b4b3c49f92c5a3484a0b7350ab352c
Vittor Dallacqua <vittordallacqua@hotmail.com>
2023-01-31 21:30:11 -0300

    update base path to fix incorrect assets links

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/vite-gh-pages",
+  base: "/vite-deploy-demo/",
   plugins: [react()],
 })
