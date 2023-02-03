d4ea34e7796f158d95da135605667107e67c1db5
Vittor Dallacqua <vittordallacqua@hotmail.com>
2023-02-03 15:39:10 -0300

    changed footer description

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,10 @@ import "../styles/components/footer.sass";
 function Footer() {
   return (
     <div className="main-footer">
-      <p> &copy;{new Date().getFullYear()} Vittor Dallacqua </p>
+      <p> &copy;{new Date().getFullYear()} Vittor Dallacqua | Desenvolvido com
+        <a target="__blank" href="https://reactjs.org/"> React.js </a> e
+        <a target="__blank" href="https://sass-lang.com/"> SCSS </a>
+      </p>
     </div>
   );
 }
