cd68ee65b39f512b6868f30d707791ccd85afee4
Vittor Dallacqua <vittordallacqua@hotmail.com>
2023-02-10 18:33:28 -0300

    changed about in the main content container

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -1,6 +1,6 @@
 const AboutContainer = () => {
   return <section className="about-container">
-    <h2>About me</h2>
+    <h2>About</h2>
     <p>Welcome! <br></br><br></br> Technical degree in informatics from TECPUC. 
     I am currently studying Systems Analysis and Development at the Pontifical Catholic University of Paraná. 
     I am looking for an opportunity to apply my knowledge in WEB development, 
