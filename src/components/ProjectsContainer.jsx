78515a9c4dd1032f47c2a0a5a65e69b5abcec214
vittor <vittordallacqua@hotmail.com>
2025-07-09 02:42:08 -0300

    fixed some responsiveness issues

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -1,7 +1,7 @@
 const ProjectsContainer = () => {
   return <section className="about-container">
     <h2>projects</h2>
-    <p>you can see my projects by clicking the button aside &nbsp;
+    <p>you can see my projects by clicking the button aside <br></br>
       <a target="_blank" href="https://github.com/vittordallacqua" className="btn">> see projects!!!</a>
     </p>
   </section>
