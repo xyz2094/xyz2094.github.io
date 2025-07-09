651194475ddfbe2fa2d43fcf93037b828f8c159f
vittor <vittordallacqua@hotmail.com>
2025-07-09 02:43:26 -0300

    fixed links

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -2,7 +2,7 @@ const ProjectsContainer = () => {
   return <section className="about-container">
     <h2>projects</h2>
     <p>you can see my projects by clicking the button aside <br></br>
-      <a target="_blank" href="https://github.com/vittordallacqua" className="btn">> see projects!!!</a>
+      <a target="_blank" href="https://github.com/xyz2094" className="btn">> see projects!!!</a>
     </p>
   </section>
 }
