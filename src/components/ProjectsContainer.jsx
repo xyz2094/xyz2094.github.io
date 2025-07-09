9ccf0bd139fec4613032707f9a32703d2b39eb74
Vittor Dallacqua <vittordallacqua@hotmail.com>
2025-07-09 11:11:54 -0300

    Update ProjectsContainer.jsx

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -3,6 +3,7 @@ const ProjectsContainer = () => {
     <h2>projects</h2>
     <p>you can see my projects by clicking the button below</p>
     <a target="_blank" href="https://github.com/xyz2094" className="btn">> see projects!!!</a>
+    <br></br>
 
   </section>
 }
