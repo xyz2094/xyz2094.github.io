17030992a326d8c9d3040287251b02e33000bf47
vittor <vittordallacqua@hotmail.com>
2025-07-09 02:43:59 -0300

    fixed some responsiveness issues v2

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -1,9 +1,9 @@
 const ProjectsContainer = () => {
   return <section className="about-container">
     <h2>projects</h2>
-    <p>you can see my projects by clicking the button aside <br></br>
-      <a target="_blank" href="https://github.com/xyz2094" className="btn">> see projects!!!</a>
-    </p>
+    <p>you can see my projects by clicking the button aside</p> <br></br>
+    <a target="_blank" href="https://github.com/xyz2094" className="btn">> see projects!!!</a>
+
   </section>
 }
 
