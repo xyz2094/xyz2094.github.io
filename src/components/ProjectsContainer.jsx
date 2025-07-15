77552b2845126232b756abe2d0c9b62c69a4e560
vittor <vittordallacqua@hotmail.com>
2025-07-15 01:17:34 -0300

    updated the projects section

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -1,11 +1,13 @@
 const ProjectsContainer = () => {
   return <section className="about-container">
     <h2> > projects</h2>
-    <a className="btn" href="https://github.com/xyz2094/zip-search">@zip-search</a>
-    <a className="btn" href="https://github.com/xyz2094/chess-game">@chess-game</a>
-    <a className="btn" href="https://github.com/xyz2094/mips-simulator">@mips-simulator</a>
-    <a className="btn" href="https://github.com/xyz2094/c-netgraph">@c-netgraph</a>
-    <a className="btn" href="https://github.com/xyz2094/xyz2094.github.io">@portfolio</a>
+    <div className="projects-list">
+      <a className="btn" href="https://github.com/xyz2094/truc-game">@truc-game</a>
+      <a className="btn" href="https://github.com/xyz2094/chess-game">@chess-game</a>
+      <a className="btn" href="https://github.com/xyz2094/mips-simulator">@mips-simulator</a>
+      <a className="btn" href="https://github.com/xyz2094/c-netgraph">@c-netgraph</a>
+      <a className="btn" href="https://github.com/xyz2094/xyz2094.github.io">@portfolio</a>
+    </div>
   </section>
 }
 
