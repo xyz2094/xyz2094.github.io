7445fd4d0b27d022480c7fc3d6b115e9ae22025f
vittor <vittordallacqua@hotmail.com>
2025-07-15 01:20:26 -0300

    added blank target for the projects hyperlinks

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -2,11 +2,11 @@ const ProjectsContainer = () => {
   return <section className="about-container">
     <h2> > projects</h2>
     <div className="projects-list">
-      <a className="btn" href="https://github.com/xyz2094/truc-game">@truc-game</a>
-      <a className="btn" href="https://github.com/xyz2094/chess-game">@chess-game</a>
-      <a className="btn" href="https://github.com/xyz2094/mips-simulator">@mips-simulator</a>
-      <a className="btn" href="https://github.com/xyz2094/c-netgraph">@c-netgraph</a>
-      <a className="btn" href="https://github.com/xyz2094/xyz2094.github.io">@portfolio</a>
+      <a target="_blank" className="btn" href="https://github.com/xyz2094/truc-game">@truc-game</a>
+      <a target="_blank" className="btn" href="https://github.com/xyz2094/chess-game">@chess-game</a>
+      <a target="_blank" className="btn" href="https://github.com/xyz2094/mips-simulator">@mips-simulator</a>
+      <a target="_blank" className="btn" href="https://github.com/xyz2094/c-netgraph">@c-netgraph</a>
+      <a target="_blank" className="btn" href="https://github.com/xyz2094/xyz2094.github.io">@portfolio</a>
     </div>
   </section>
 }
