fedd6411849de54c230f266b8c98c7596f4586dc
Vittor Dallacqua <vittordallacqua@hotmail.com>
2025-07-16 15:20:29 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ function Footer() {
   return (
     <div className="main-footer">
       <p> vittor dallacqua &copy; {new Date().getFullYear()} </p>
-      <p className="easter-egg">you clicked {count} times xD</p>
+      <p className="easter-paragraph">you clicked {count} times xD</p>
       <button className="easter-egg" onClick={() => setCount(count + 1)}> easter egg</button>
     </div>
   );
