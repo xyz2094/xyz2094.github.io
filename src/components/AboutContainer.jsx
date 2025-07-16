d4d0f71dbda546918d6bb007437ddd7a4de34d9b
Vittor Dallacqua <vittordallacqua@hotmail.com>
2025-07-16 10:11:34 -0300

    Update AboutContainer.jsx

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -1,9 +1,9 @@
 const AboutContainer = () => {
   return <section className="about-container">
     <h2> > about</h2>
-    <p>interested in data analysis
-      and full-stack development ~ i like metal music and
-      go out with my friends ~ i love my wife </p>
+    <p>
+      using unusual ideas to build great things :) 
+    </p>
   </section>
 }
 
