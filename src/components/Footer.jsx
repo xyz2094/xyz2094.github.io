71e4e5026abbe1b8eeaebd2fa8c1bbc08004245f
Vittor Dallacqua <vittordallacqua@hotmail.com>
2025-07-16 16:21:25 -0300

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,13 +5,15 @@ import "../styles/components/footer.sass";
 function Footer() {
   const [count, setCount] = useState(0)
   return (
-    <div className="main-footer">
-      <p> vittor dallacqua &copy; {new Date().getFullYear()} </p>
-    </div>
-    <div className="easter-egg">
-      <p>you clicked {count} times xD</p>
-      <button onClick={() => setCount(count + 1)}>easter egg</button>
-    </div>
+    <> 
+      <div className="main-footer">
+        <p> vittor dallacqua &copy; {new Date().getFullYear()} </p>
+      </div>
+      <div className="easter-egg">
+        <p>you clicked {count} times xD</p>
+        <button onClick={() => setCount(count + 1)}>easter egg</button>
+      </div>
+    </>
   );
 }
 
