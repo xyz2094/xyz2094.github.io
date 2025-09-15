2c1ff6113976a110a91fac6fbe61c51f23457d96
Vittor Dallacqua <vittordallacqua@hotmail.com>
2025-09-15 16:13:30 -0300

    Update ProjectsContainer.jsx

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -7,7 +7,7 @@ const ProjectsContainer = () => {
       <a target="_blank" className="btn" href="https://github.com/xyz2094/mips-simulator">@mips-simulator</a>
       <a target="_blank" className="btn" href="https://github.com/xyz2094/c-netgraph">@c-netgraph</a>
       <a target="_blank" className="btn" href="https://github.com/xyz2094/xyz2094.github.io">@portfolio</a>
-      <a target="_blank" className="btn" href="https://xyz2094.github.io/chaosfanatic">@chaosfanatic</a>
+      <a target="_blank" className="btn" href="www.chaosfanatic.com.br">@chaosfanatic</a>
     </div>
   </section>
 }
